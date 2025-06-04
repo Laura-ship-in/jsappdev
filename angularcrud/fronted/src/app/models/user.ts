bf38c3489f759202fe866167d6a4f9e65f49e2b1
Laura <elaura39@yahoo.com>
2025-06-04 20:59:53 +0300

    angularcru

---
+++ angularcrud/fronted/src/app/models/user.ts
@@ -0,0 +1,14 @@
+export interface User {
+        id: number;
+        nume: string;
+        prenume: string;
+        datanastere: Date; //string
+        email: string;
+        telefon: string;
+        dataadaugare?: Date;
+        cnp?: string;
+        poza?: string;
+        actiune?: string;
+        users?: any;
+      }
+
